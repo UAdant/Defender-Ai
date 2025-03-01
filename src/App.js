26b0601f370c1356556a2610b809c6c60441c748
UAdant <pbit157@gmail.com>
2025-03-01 19:22:08 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="header">Чим ми можемо Вам допомогти?</h1>
+      <div className="search-container">
+        <input type="text" className="search-input" placeholder="Запитайте щось..." />
+        <div className="icons">
+          <img src="/icon-search.png" alt="Пошук" className="icon" />
+          <img src="/icon-history.png" alt="Додатковий значок" className="icon" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
