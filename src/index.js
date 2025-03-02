e3520be03735b849a26c0df99e67440b10605f93
Lainy01 <xolylain@gmail.com>
2025-03-02 09:42:34 +0200

    push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Mainpage from './mainpage';
+import Mainpage from './Mainpage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/mainpage.js
+++ src/mainpage.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import App from './App'; // Імпортуємо App
 import './Mainpage.css';
 
 const Mainpage = () => {
   return (
-    <div className="container">
-      <img src="/bcg2.png" alt="Фон" className="background"></img>
-      <div className="title-container">
-        <img src="/logo.png" alt="Логотип" className="icon"/>
-      <h1 className="header">Defender AI</h1>
-      </div>
-      <div className="equipments">
-        <img src="/detail1.png" alt="Шолом" className="icon2"/>
-      <img src="/detail2.png" alt="Амуніція" className="icon3"/>
-      </div>
-      
-      <Router>
-      <div className="action">
-        <Link to="/app">
-          <button className="button1">Почати</button>
-        </Link>
+    <Router>
+      <div className="container">
+        <img src="/bcg2.png" alt="Фон" className="background"></img>
+        <div className="title-container">
+          <img src="/logo.png" alt="Логотип" className="icon"/>
+          <h1 className="header">Defender AI</h1>
+        </div>
+        <div className="equipments">
+          <img src="/detail1.png" alt="Шолом" className="icon2"/>
+          <img src="/detail2.png" alt="Амуніція" className="icon3"/>
+        </div>
+        
+        <div className="action">
+          <Link to="/app">
+            <button className="button1">Почати</button>
+          </Link>
+        </div>
 
+        {/* Маршрутизація */}
         <Routes>
-          <Route path="/app" element={<Mainpage />} />
+          <Route path="/app" element={<App />} />
         </Routes>
       </div>
     </Router>
-    </div>
   );
 };
 
 export default Mainpage;
+
